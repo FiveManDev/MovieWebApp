e2b4895570fc8254e0c66ebce0bb35592dd1aabd
lenhatquynh <nhatquynh24092001@gmail.com>
2022-09-22 00:08:12 +0700

    feat(pages): add landing page

---
+++ MovieWebApp/MovieWebApp/Pages/Forgot-password/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MovieWebApp.Pages.Forgot_password
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
