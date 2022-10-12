ca20dcc9285124252f27ff1b59bd4691d7d23057
Nguyen Hoang Khang <hoangkhang12789@gmail.com>
2022-10-12 23:44:34 +0700

    add code SignalR

--- MovieWebApp/MovieWebApp/Pages/Contact/Index.cshtml.cs
+++ MovieWebApp/MovieWebApp/Pages/Contact/Index.cshtml.cs
@@ -1,12 +1,25 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace MovieWebApp.Pages.Contact
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            try
+            {
+                var userId = (User.Identity as ClaimsIdentity).FindFirst("UserID").Value;
+                TempData["UserID"] = userId;
+                return Page();
+            }
+            catch
+            {
+                return Redirect("/ErrorPage?statusCode=404");
+            }
         }
     }
 }
