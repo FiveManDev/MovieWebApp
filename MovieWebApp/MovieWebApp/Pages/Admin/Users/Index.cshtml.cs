690dcf1a93ca7803082d734aa1fb028f43aab480
lenhatquynh <nhatquynh24092001@gmail.com>
2022-10-31 21:00:15 +0700

    feat(API): mock api catalog, review

--- MovieWebApp/MovieWebApp/Pages/Admin/Users/Index.cshtml.cs
+++ MovieWebApp/MovieWebApp/Pages/Admin/Users/Index.cshtml.cs
@@ -19,6 +19,10 @@ namespace MovieWebApp.Pages.Admin.Users
         public string changeStatusUser { get; set; }
         public string deleteUser { get; set; }
         public int GetTotalUser { get; set; }
+        [BindProperty]
+        public string UserSort { get; set; }
+        [BindProperty]
+        public string UserSearch { get; set; }
 
         public IndexModel(UserServices userServices, ProfileServices profileServices, StatisticsServicess statisticsServicess)
         {
@@ -31,7 +35,55 @@ namespace MovieWebApp.Pages.Admin.Users
             var userId = (User.Identity as ClaimsIdentity).FindFirst("UserID").Value;
             UserDTO = await _profileServices.GetInformation(HttpContext, userId);
 
+            UserSort = "";
+            UserSearch = "";
+
             GetUsersDTOs = await _userServices.GetUsers(HttpContext, "", "", "");
+
+            GetTotalUser = await _statisticsServicess.GetTotalUser(HttpContext);
+
+            changeStatusUser = Request.Query["changeStatusUser"].ToString();
+            deleteUser = Request.Query["deleteUser"].ToString();
+
+            if (changeStatusUser != "")
+            {
+                UserChangeDTO = await _profileServices.GetInformation(HttpContext, changeStatusUser);
+                ChangeStatusUserDTO update = new ChangeStatusUserDTO();
+                update.UserID = changeStatusUser;
+                update.IsBanned = !UserChangeDTO.status;
+                var result = await _userServices.ChangeStatusUser(HttpContext, update);
+                if (result)
+                {
+                    TempData["success"] = "Change status user successfully!";
+                }
+                else
+                {
+                    TempData["error"] = "Change status user fail!";
+                }
+                return RedirectToPage("/Admin/Users/Index");
+            }
+            if (deleteUser != "")
+            {
+                var result = await _userServices.DeleteUser(HttpContext, deleteUser);
+                if (result)
+                {
+                    TempData["success"] = "Delete user successfully!";
+                }
+                else
+                {
+                    TempData["error"] = "Delete user fail!";
+                }
+                return RedirectToPage("/Admin/Users/Index");
+            }
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPostSearchSort()
+        {
+            var userId = (User.Identity as ClaimsIdentity).FindFirst("UserID").Value;
+            UserDTO = await _profileServices.GetInformation(HttpContext, userId);
+
+            GetUsersDTOs = await _userServices.GetUsers(HttpContext, UserSearch, UserSort, "");
 
             GetTotalUser = await _statisticsServicess.GetTotalUser(HttpContext);
 
